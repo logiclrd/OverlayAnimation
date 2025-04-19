d1f0745180d3cc6d38114e7a13df5a1aa8e329c5
Jonathan Gilbert <logic@deltaq.org>
2025-04-19 02:33:52 -0500

    Initial commit

---
+++ Parameters.cs
@@ -0,0 +1,12 @@
+public class Parameters
+{
+	public int FrameCount;
+	public int RenderWidth;
+	public int RenderHeight;
+	public int BugCount;
+	public float BugSize;
+	public float BugThickness;
+	public float RepulsionStrength;
+	public float MaximumDistance;
+	public float Jitter;
+}
